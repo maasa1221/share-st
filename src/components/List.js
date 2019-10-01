034c4d25ac93b66e85d3cbf7e7ddfcde072fd06a
渡辺　昌 <maasa1221@gmail.com>
2019-10-01 18:32:03 +0900

    list(probox) update

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Link} from "react-router-dom"
+import freeee from './freeee.png'
+import freeee_logo from './freeee_logo.png'
+import new_tag from './new_tag.png'
 const tempAvatar = 'https://firebasestorage.googleapis.com/v0/b/novels-a5884.appspot.com/o/temp%2Ftemp.png?alt=media&token=a4d36af6-f5e8-49ad-b9c0-8b5d4d899c0d'
 
 
@@ -9,13 +12,19 @@ const Contents_Box = (data) => {
   var list = [];
   for(var i in data){
     list.push(
-      <Probox>
-        <Link to = {`/detail/${data[i].id}`} >
-        <Proimg src={data[i].image}/>  
-        <Texttitle>{data[i].content_title}</Texttitle>
-        <Textname>{data[i].company_name}</Textname>
-        </Link>
-      </Probox>
+      <Link to = {`/detail/${data[i].id}`} style={{ textDecoration: 'none' }} >
+        <Probox>
+          <ImagePhoto src={data[i].image}/> 
+          <Comp_about_box>
+            <Tag src={data[i].tag}/> 
+            <Texttitle>{data[i].content_title}</Texttitle>
+            <Comp_profile>
+              <Proimg src={data[i].logo}/>
+              <Textname>{data[i].company_name}</Textname>
+            </Comp_profile>
+          </Comp_about_box>
+        </Probox>
+      </Link>
     );
   }
   return(
@@ -28,16 +37,27 @@ const Contents_Box = (data) => {
 class ProList extends React.Component {
 
   state={
-    list_square:[{
-      image:tempAvatar,
-      content_title:"インドアセールスの設計の仕方",
-      company_name:"Gifts Japan",
-    },
-    {
-      image:tempAvatar,
-      content_title:"インドアセールスの設計の仕方",
-      company_name:"Gifts Japan",
-    }]
+    list_square:[
+        {
+        image:freeee,
+        tag:new_tag,
+        logo:freeee_logo,
+        content_title:"会社を伸ばす攻めのバックオフィスクラウドERP",
+        company_name:"freeee 株式会社",
+        },
+        {
+          image:tempAvatar,
+          tag:new_tag,
+          content_title:"インドアセールスの設計の仕方",
+          company_name:"Gifts Japan",
+        },
+        {
+          image:freeee,
+          tag:new_tag,
+          content_title:"会社を伸ばす攻めのバックオフィスクラウドERP",
+          company_name:"freeee 株式会社",
+          },
+      ]
   }
   render() {
     return (
@@ -50,30 +70,61 @@ class ProList extends React.Component {
 
 const Proimg = styled.img`
   border-radius: 50%; 
-  width:  150px;       
-  height: 150px; 
+  width:  50px;       
+  height: 50px; 
+  position: relative;
+  margin-right: 15px;
+`;
+
+const ImagePhoto = styled.img`
+  height: 200px;
   position: relative;
 `;
+
+const Tag = styled.img`
+  display: block;
+  height: 30px;
+  position: relative;
+`;
+
 const List_container = styled.div`
-  width: 80%;
+  width: 70%;
   margin: auto;
 `;
 const Texttitle = styled.p`
   font-size: 25px;
   position: relative;
+  color: #000;
 `;
 const Textname = styled.p`
   font-size: 20px;
+  display: flex;
+  align-items: center;
+  height: 50px;
   position: relative;
+  color: #000;
 `;
 const Probox = styled.div`
-  margin: 10px;
-  padding: 5px;
-  border: solid #ddd;
-  width: 30%;
+  margin: 20px auto 40px;
+  padding: 40px;
+  border-bottom: solid #ddd;
+  width: 60%;
   text-align:center;
-  display: inline-block;
+  display: block;
+  &:hover {
+    background-color: #eee;
+    color: white;
+    text-decoration: none;}
 `;
 
+const Comp_profile = styled.div`
+  display: inline-box;
+`;
+
+const Comp_about_box = styled.div`
+width: 80%;
+margin: 20px auto 0px;
+text-align:left;
+`;
 
 export default ProList;
