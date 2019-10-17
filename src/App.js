49ae46e31527720bc9b55b3d1c783d728107d4cc
渡辺　昌 <maasa1221@gmail.com>
2019-10-17 14:09:03 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router,Route} from "react-router-dom"
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import HomeScreen from './screens/HomeScreen';
-import ListScreen from './screens/ListScreen';
-import CreateScreen from './screens/CreateScreen';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeScreen from "./screens/HomeScreen";
+import ListScreen from "./screens/ListScreen";
+import CreateScreen from "./screens/CreateScreen";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const AppNavigator = () =>(
-  <Router>
+const AppNavigator = () => (
+  <Router basename={process.env.PUBLIC_URL}>
     <div>
-      <Route exact path="/" component = {HomeScreen}/>
-      <Route path="/list" component = {ListScreen}/>
-      <Route path="/create" component = {CreateScreen}/>
+      <Route exact path="/" component={HomeScreen} />
+      <Route path="/list" component={ListScreen} />
+      <Route path="/create" component={CreateScreen} />
     </div>
   </Router>
-)
+);
 
 class App extends React.Component {
-  
   render() {
     return (
       <div>
